073fe5034bd356deb3e385a5f27116137dd8f806
Andres Martinez <amartinez@andymartinez.tech>
2021-11-12 11:02:56 -0500

    Changed terraform/version.tf file to switch to new arm64 compatible release of the proxmox provider

--- terraform/version.tf
+++ terraform/version.tf
@@ -1,9 +1,8 @@
 terraform {
   required_providers {
     proxmox = {
-      source  = "telmate/proxmox"
-      version = ">=1.0.0"
+      source = "telmate/proxmox"
+      version = "2.9.0"
     }
   }
-  required_version = ">= 0.14"
 }
