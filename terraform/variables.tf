4f1768a989165177bdd91465d9e778e956032105
Andres Martinez <amartinez@andymartinez.tech>
2021-12-24 10:05:08 -0500

    Added awx server to ansible inventory file, modified template being used in terraform code

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,36 +2,16 @@ variable "virtual_machines" {
     description = "Map of Virtual Machines to be created"
     type        = map(any)
     default     = {
-        "ms01" = {
-            name = "ms01"
+        "test" = {
+            name = "test-vm"
             clone = "ubuntu-20.04-cloudimg"
             full_clone =  true
             memory = 8192
             cores = 8
             disk = "60G"
             ip = "10.1.10.15"
-            agent = 1
+            agent = 1  
         }
-        "dev_box" = {
-            name = "dev-box"
-            clone = "ubuntu-2004-cloudinit-template"
-            full_clone = true
-            memory = 8192
-            cores = 4
-            disk = "80G"
-            ip = "10.1.10.22"
-            agent = 0
-        }
-        "awx" = {
-            name = "awx"
-            clone = "ubuntu-2004-cloudinit-template"
-            full_clone = true
-            memory = 8192
-            cores = 4
-            disk = "60G"
-            ip = "10.1.10.23"
-            agent = 0
-        }    
     }
 }
 variable "pm_api_url" {
@@ -46,9 +26,7 @@ variable "pm_pass" {
 }
 variable "dns_domain" {
     type = string
-    default = "internal.themartinez.house"
 }
- variable "dns_ip" {
+variable "dns_ip" {
      type = string
-     default = "10.1.10.5"
  }
